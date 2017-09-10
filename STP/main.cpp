48fbf0ce80a7bd58c6b3a0b9b9ab6e88a0297470
deman2332 <dimas97@mail.ru>
2017-09-10 14:55:13 +0300

    completed

--- STP/main.cpp
+++ STP/main.cpp
@@ -6,12 +6,24 @@
 #define FILE "text.txt"
 int main(int argc, char** argv) {
 	std::string file;
+
+	for (int i = 0; i < argc; i++) {
+		std::cout << "argv["<<i<<"]= " << argv[i] << '\n';
+	}
+	std::cout << "\n\n";
+
 	if (argc == 2)
 		file = argv[1];
 	else
 		file = FILE;
 
 	TextParserSTP* parser = TextParserSTP::getInstance(file);
+
+	if (parser == nullptr) {
+		std::cout << "invalid path: " << file<<'\n';
+		system("pause");
+		return -1;
+	}
 	parser->parse();
 	STP*stp = parser->getSTP();
 	stp->sendMessageFromAllBridges();
